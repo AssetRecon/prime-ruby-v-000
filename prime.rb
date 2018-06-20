3d1ba2a73546906dd7d7e782fd7f13406926086e
Edsel Caprice <caprice@mail.usf.edu>
2018-06-20 23:50:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,5 @@ def prime?(num)
 
 return false if num == 0 || num == 1
 
-Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-  true
+(2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
