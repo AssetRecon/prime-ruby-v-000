078bfe25fa1bdb43dc3e3d466a0892ee6bc71c53
Edsel Caprice <caprice@mail.usf.edu>
2018-06-20 23:46:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ return false if int == 0 || int == 1
 
 2.upto(Math.sqrt(num).ceil) do |i|
         break if num%i==0
-        return true if i==Math.sqrt(num).ceil   
+        return true if i==Math.sqrt(num).ceil
     end
     return false
 
