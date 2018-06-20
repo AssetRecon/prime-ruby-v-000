6223aa552edb5575149cd69fdecca1490dde20fa
Edsel Caprice <caprice@mail.usf.edu>
2018-06-20 23:47:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,13 @@ def prime?(num)
 
 return false if num == 0 || num == 1
 
-2.upto(Math.sqrt(num).ceil) do |i|
-        break if num%i==0
-        return true if i==Math.sqrt(num).ceil
-    end
-    return false
-
+2.upto(Math.sqrt(n)) do |x| # testing up to the square root of the number because going past there is excessive
+   if n % x == 0
+     # n is the number being called from the program
+     # x is the number we're dividing by, counting from 2 up to the square root of the number
+     return false # this means the number is not prime
+   else
+     return true # this means the number is prime
+   end 
+ end
 end
