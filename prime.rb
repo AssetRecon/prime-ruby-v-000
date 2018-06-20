7114fd320a6c81929821ba5313b78c585d8d0156
Edsel Caprice <caprice@mail.usf.edu>
2018-06-20 23:32:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(int)
 
 return false if int == 0 || int == 1
-Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+Math.sqrt(int).to_i.downto(2).each {|i| return false if num % i == 0}
   true
 
 end
